be66c10096e707d4bdee9af3595046d5081ed2e7
Kucherenko <evgeny.kucherenko004@gmail.com>
2025-06-08 20:52:14 +0200

    fix

--- services/api/chat.ts
+++ services/api/chat.ts
@@ -1,6 +1,6 @@
 import { ChatPayload } from '@/types/types';
 
-const API_BASE_URL = 'http://35.180.166.154:8000';
+const API_BASE_URL = 'https://35.180.134.138:8000';
 
 export async function sendChatMessage(payload: ChatPayload) {
   const response = await fetch(`${API_BASE_URL}/chat`, {
